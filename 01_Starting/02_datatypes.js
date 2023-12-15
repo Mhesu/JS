0a3f7ad362c3e3a21c6f415dcb67a396f51a1390
Mhesu <153717144+Mhesu@users.noreply.github.com>
2023-12-15 17:07:44 +0000

    datatypes

---
+++ 01_Starting/02_datatypes.js
@@ -0,0 +1,20 @@
+let name = "Mahesh"
+let number = 10
+let isLoggedin = true
+let mobNo = 9680939082
+
+console.table([name, number, isLoggedin, mobNo])
+
+/*
+string = ""
+symbol = for uniqueness
+number = 1
+null = standalone value
+bigint = 123456789
+boolean = true/false
+undefined = undefined
+*/
+console.log(typeof "Mahesh")
+console.log(typeof isLoggedin)
+console.log(typeof null) // output => object
+console.log(typeof undefined) // output => undefined
