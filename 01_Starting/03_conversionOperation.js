04e9ae2c34089221f04e70af79d10927f51cc511
Mhesu <153717144+Mhesu@users.noreply.github.com>
2023-12-15 17:39:54 +0000

    conversionOperation

---
+++ 01_Starting/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let age = undefined
+
+// console.log(typeof 18)
+// console.log(typeof (age))
+
+let valueInNumber = Number(age)
+// console.log( typeof valueInNumber)
+// console.log(valueInNumber) 
+
+/*
+ 18 => 18 (number)
+ "18" => 18 (NaN [NotaNumber])
+ true => 1; false => 0
+ null => 0 
+ undefined => NaN
+*/
+
+let isLoggedIn = "Mahesh"
+
+// console.log(typeof isLoggedIn);
+
+let booleanisLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+/*
+ 1 => true
+ 0 => false
+ "" => false
+ "Mahesh" => true
+*/
+
+let num = 18
+
+// console.log(typeof num);
+let stringNumber =  String(num)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
