7364091edbf717c9fe5d03c90812ce955c781558
Mhesu <153717144+Mhesu@users.noreply.github.com>
2023-12-16 09:34:24 +0000

    conversionOperation

--- 01_Starting/03_conversionOperation.js
+++ 01_Starting/03_conversionOperation.js
@@ -33,8 +33,44 @@ let num = 18
 
 // console.log(typeof num);
 let stringNumber =  String(num)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
 
 
+/* **************************************Operations************************************** */
+
+let val = 8
+let negativeVal = -val
+// console.log(negativeVal);
+
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2**3)
+// console.log(2%3)
+
+let str1 = "Hello "
+let str2 = "Mahesh"
+let strResult = str1 + str2
+// console.log(strResult);
+
+// console.log(1 + 2)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log(1 + 2 + "3")
+
+// console.log( (2 + 3) * 4 ** 5 / 6)
+
+// console.log(+true)
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(--gameCounter);
